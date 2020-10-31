249108cafac6712c7e78c90629ee3739e3a2b082
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2020-10-31 09:57:42 -0400

    addition of script.js, random gen variables and global variables

---
+++ script.js
@@ -0,0 +1,85 @@
+// variables to be randomly selected
+var lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var uppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var special = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "~",
+    "&",
+    "*",
+    "(",
+    ")",
+    "+",
+    "?",
+    ",",
+    ".",
+    ";",
+    "[",
+    "]",
+  ];
+  // global variables
+  var passwordLength;
+  var uppercaseC;
+  var lowercaseC;
+  var numberC;
+  var specialC;
+  // empty string
+  var possibleChar = [];
